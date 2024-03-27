9058b691cf6e5a3506e472ea14b24d6d19881e22
Flog7877 <flog7877@gmail.com>
2024-03-27 18:55:09 +0100

    Dropdown Auswahlfenster hinzugefügt für Stadtteile (funktioniert)

--- therapieliste.js
+++ therapieliste.js
@@ -303,7 +303,30 @@ formTest.addEventListener('submit', (e) => {
 
     console.log(werte);
 }) /// klappt einfach lol
-*/
+// Dropdownmenü*/
+
+let checkList = document.getElementById('dropdown-stadt');
+var items = document.getElementById('items');
+        checkList.getElementsByClassName('anchor')[0].onclick = function (evt) {
+            if (items.classList.contains('visible')){
+                items.classList.remove('visible');
+                items.style.display = "none";
+            }
+            
+            else{
+                items.classList.add('visible');
+                items.style.display = "block";
+            }
+            
+            
+        }
+
+        items.onblur = function(evt) {
+            items.classList.remove('visible');
+        }
+
+// Hier beginnt der querySelector für dir form
+
 const formTest = document.querySelector('form');
 
 formTest.addEventListener('submit', (e) => {
